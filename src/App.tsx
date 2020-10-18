361ba042529ca42c1ab3956788f7e2be7215bab4
Egor Malyutin <abv1236@yandex.ru>
2020-10-18 19:17:11 +0600

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import {
   SwipeableDrawer,
+  Drawer,
+  Hidden,
   Button,
   List,
   ListItem,
@@ -11,13 +13,16 @@ import {
   Container,
 } from "@material-ui/core";
 
-import { MoveToInbox, Mail } from "@material-ui/icons";
+import { MoveToInbox, Mail, Usb } from "@material-ui/icons";
 
 import SwipeableView from "react-swipeable-views";
 
+// Components
 import Login from "./Login";
 import Map from "./Map";
-import CreateAvatar from "./CreateAvatar";
+import CreateAvatar, { Avatar } from "./CreateAvatar";
+
+const xsDrawerWidth = 200;
 
 const useStyles = makeStyles({
   container: {
@@ -27,13 +32,33 @@ const useStyles = makeStyles({
     height: "100%",
     maxHeight: "100%",
   },
+
+  main: {
+    display: "flex",
+  },
+
+  xsDrawer: {
+    flexShrink: 0,
+    width: xsDrawerWidth,
+  },
+
+  xsDrawerPaper: {
+    width: xsDrawerWidth,
+  },
+
+  content: {
+    flexGrow: 1,
+    padding: 30,
+  },
 });
 
+// Context keeping info about user
 type UserData = {
   image: string;
 };
 
-function MeetScreen({ finish }: { finish: (data: UserData) => void }) {
+// Meet screens, including login screen, map screen, and avatar screen.
+function MeetScreens({ finish }: { finish: (data: UserData) => void }) {
   const classes = useStyles();
   const [screen, setScreen] = useState(0);
 
@@ -46,6 +71,7 @@ function MeetScreen({ finish }: { finish: (data: UserData) => void }) {
     setScreen((s) => (s > 0 ? s - 1 : s));
   }, []);
 
+  // Make back button work
   useEffect(() => {
     window.addEventListener("popstate", pop);
     return () => {
@@ -64,7 +90,7 @@ function MeetScreen({ finish }: { finish: (data: UserData) => void }) {
       containerStyle={{ height: "100%" }}
       style={{ height: "100%" }}
     >
-      <Login onLogIn={push} />
+      <div>{screen == 0 ? <Login onLogIn={push} /> : null}</div>
 
       <div className={classes.mapContainer}>
         <Map next={push} />
@@ -75,38 +101,93 @@ function MeetScreen({ finish }: { finish: (data: UserData) => void }) {
   );
 }
 
-export default function App() {
-  const classes = useStyles();
-  const [anchor, setAnchor] = useState(false);
+// Menu screen icons
+const menuScreens = [<MoveToInbox />, <Mail />, <Usb />];
 
+function Menu({
+  data: { image },
+  onSelected,
+}: {
+  data: UserData;
+  onSelected: (item: number) => void;
+}) {
+  return (
+    <List>
+      <Avatar size="150px">
+        <img src={image} />
+      </Avatar>
+
+      {menuScreens.map((icon, index) => (
+        <ListItem button key={index} onClick={() => onSelected(index + 1)}>
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText primary={`Экран ${index + 1}`} />
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+function AppContent({ data }: { data: UserData }) {
+  const classes = useStyles();
+
+  const [anchor, setAnchor] = useState(false);
   const setDrawer = (to: boolean) => () => setAnchor(to);
 
+  const [screen, setScreen] = useState(1);
+
+  const menu = <Menu data={data} onSelected={setScreen} />;
+
+  // Use swipeable drawer on mobile screens and permanent drawer otherwise
   return (
-    <MeetScreen finish={() => console.log(123)} />
-    // <div>
-    // <Button onClick={setDrawer(true)}>Left</Button>
-    // <SwipeableDrawer
-    // anchor={"left"}
-    // open={anchor}
-    // onClose={setDrawer(false)}
-    // onOpen={setDrawer(true)}
-    // >
-    // <Container
-    // className={classes.container}
-    // role="presentation"
-    // onClick={setDrawer(false)}
-    // onKeyDown={setDrawer(false)}
-    // >
-    // <List>
-    // {[<MoveToInbox />, <Mail />].map((icon, index) => (
-    // <ListItem button key={index}>
-    // <ListItemIcon>{icon}</ListItemIcon>
-    // <ListItemText primary={`Экран ${index}`} />
-    // </ListItem>
-    // ))}
-    // </List>
-    // </Container>
-    // </SwipeableDrawer>
-    // </div>
+    <div className={classes.main}>
+      <Hidden smUp implementation="css">
+        <SwipeableDrawer
+          anchor={"left"}
+          open={anchor}
+          onClose={setDrawer(false)}
+          onOpen={setDrawer(true)}
+        >
+          <Container
+            className={classes.container}
+            role="presentation"
+            onClick={setDrawer(false)}
+            onKeyDown={setDrawer(false)}
+          >
+            {menu}
+          </Container>
+        </SwipeableDrawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <div className={classes.xsDrawer}>
+          <Drawer
+            classes={{ paper: classes.xsDrawerPaper }}
+            variant="permanent"
+            open
+          >
+            {menu}
+          </Drawer>
+        </div>
+      </Hidden>
+      <div className={classes.content}>Экран №{screen}</div>
+    </div>
+  );
+}
+
+export default function App() {
+  const classes = useStyles();
+
+  // Switch screens
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [screen, setScreen] = useState<"meet" | "content">("meet");
+
+  return screen == "meet" ? (
+    <MeetScreens
+      finish={(data) => {
+        setUserData(data);
+        setScreen("content");
+      }}
+    />
+  ) : (
+    <AppContent data={userData!} />
   );
 }
